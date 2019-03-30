f7160ffbebcb423c81e01a38b571fdfd5b375af7
Bernard Shih <bernard@Stormrage.local>
2019-03-30 12:53:49 +0800

    updated keymap

--- config.h
+++ config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 Danny Nguyen <danny@keeb.io>
+Copyright 2019 Bernard Shih (twitter: @SA_EndlessGame)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -3,13 +3,11 @@
 #define _BASE 0
 #define _LOWER 1
 #define _RAISE 2
-#define _ADJUST 16
 
 enum custom_keycodes {
   BASE = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST,
 };
 
 enum tapdances{
@@ -26,7 +24,6 @@ enum tapdances{
 #define KC_N0BS  TD(TD_N0BS)
 #define KC_RPPI  TD(TD_RPPI)
 
-
 #define KC_BSCT LCTL_T(KC_BSPC)
 #define KC_CTEN LCTL(KC_ENT)
 #define KC_ALEN LALT(KC_ENT)
@@ -37,7 +34,7 @@ enum tapdances{
 #define KC_DSTP LGUI(KC_D)
 
 #define KC_ESLO LT(LOWER, KC_ESC)
-#define KC_L2RA LT(RAISE, KC_ALEN)
+#define KC_L2RA LT(RAISE, KC_WLNG)
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_SCCL] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_QUOT),
@@ -56,17 +53,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
              KC_SCEN,          KC_ESLO, KC_BSCT, KC_BSCT, KC_SPC,  KC_SPC,  KC_L2RA
   ),
 
-  [_RAISE] = LAYOUT(
-             KC_1,    KC_2,    KC_3,      KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_N0BS, \
-             KC_F1,   KC_F2,   KC_F3,     KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, \
-             KC_F7,   KC_F8,   KC_F9,     KC_F10,  KC_F11,  KC_F12,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,\
-             KC_CTEN,          KC_LOCK,   KC_BSCT, KC_BSCT, KC_SPC,  KC_SPC,  _______
-  ),
-
   [_LOWER] = LAYOUT(
              KC_EXLM, KC_AT,   KC_HASH,   KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPPI, \
-             KC_F1,   KC_F2,   KC_DSTP,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,  \
-             KC_F7,   KC_F8,   KC_F9,     KC_F10,  KC_F11,  KC_F12,  KC_PGUP, KC_PGDN ,KC_HOME, KC_END,\
-             KC_CTEN,          _______,   KC_BSCT, KC_BSCT, KC_SPC,  KC_SPC,  KC_ULCK
+             _______, _______, KC_DSTP,   _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,  \
+             _______, _______, _______,   _______, _______, _______, KC_INS,  _______, KC_HOME, KC_END,\
+             KC_ALEN,          _______,   KC_BSCT, KC_BSCT, KC_SPC,  KC_SPC,  KC_ULCK
   ),
+
+  [_RAISE] = LAYOUT(
+             KC_1,    KC_2,    KC_3,      KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_N0BS, \
+             _______, _______, _______,   _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, \
+             _______, _______, _______,   _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,\
+             KC_SCEN,          KC_LOCK,   KC_BSCT, KC_BSCT, KC_SPC,  KC_SPC,  _______
+  ),
+
+
 };
