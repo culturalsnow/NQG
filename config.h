17469640a44587ba4a0b6f3dd8097a78a42c11a4
Bernard Shih <Nard@Lightbringer.local>
2019-04-02 12:16:35 +0800

    new keymap

--- config.h
+++ config.h
@@ -39,4 +39,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
-#define TAPPING_TERM 300
+#define TAPPING_TERM 200
